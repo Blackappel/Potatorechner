390ee810e9aefc66b01975bd1d186a32ccc4649f
Lukas Steen <lcs.blackapple@gmail.com>
2023-06-04 17:18:19 +0200

    ein bissl mehr 2

--- src/input.c
+++ src/input.c
@@ -92,6 +92,7 @@ void * next (void * old) {
                 PROCESS_SYMBOLES(4,ROT)
             } else
             if ( X_IN_AB(g_string[g_pos], 'a', 'z') ) {
+                PROCESS_SYMBOLES(0, VAR)
                 char * varStart = g_string[g_pos],
                     * varName,
                     replacedSymbol = '\0';
@@ -105,11 +106,11 @@ void * next (void * old) {
                 varName = (char*)malloc(varLength * sizeof(char));
                 strcpy(varName, varStart);
                 g_string[g_pos] = replacedSymbol;
-                temp->type        = VAR;
                 temp->ext.VAR_val = varName;
             } else
             if ( X_IN_AB(g_string[g_pos], '0', '9') 
                     /* || X_IN_AB(g_string[g_pos], 'A', 'F') */) { // Andere BASEN Auser 10 wenn wir mal Bock haben
+                PROCESS_SYMBOLES(0, NUM)
                 Bool isDecimal = bool_false;
                 double number = 0.0;
                 char * numberStart = g_string[g_pos];
@@ -118,7 +119,6 @@ void * next (void * old) {
                     g_pos++;
                 }
                 number = strtod(numberStart, NULL);
-                temp->type        = NUM;
                 temp->ext.NUM_val = number;
             } else
             if (g_string[g_pos] == '\0') {
